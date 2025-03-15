6cbca0a25c9addd5a8432de0c5ae1a42c134b746
puredevotion <mail@thijsbaars.nl>
2025-03-15 11:23:21 +0100

    added purgecss

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
 
+import purgecss from "astro-purgecss";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-tut2.netlify.app",
-  integrations: [preact()]
+  integrations: [preact(), purgecss()]
 });
